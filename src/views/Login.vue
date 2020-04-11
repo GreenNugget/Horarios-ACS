683a422a30c4f98716d0a57c38bcf77ec368729c
Chesare22 <Chesare22@github.com>
2020-04-10 19:15:11 -0500

    chore: Add sass

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,10 @@
   <div>
     <h1>Login Page</h1>
     <br>
-    <form />
+    <div class="working-with-scss">
+      Checando si scss funciona <br>
+      <span class="nested">Elemento anidado</span>
+    </div>
   </div>
 </template>
 
@@ -12,6 +15,16 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+.working {
+  &-with-scss {
+    font-size: 200%;
+    color: green;
 
+    .nested {
+      font-size: 60%;
+      color: grey;
+    }
+  }
+}
 </style>
